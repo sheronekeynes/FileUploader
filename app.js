763c9c437f3a4245c6751998c894d5b931f00a73
Sherone <keynessherone@gmail.com>
2025-12-19 11:37:22 +0530

    add prisma setup

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
+import session from "express-session";
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import bcrypt from "bcrypt";
+import { Pool } from "pg";
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import homeRouter from "./router/homeRouter.js";
 
 dotenv.config();
 
@@ -10,6 +18,14 @@ const port = process.env.PORT || 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+//const { PrismaClient } = pkg;
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
 // middlewares
 app.use(express.json());
@@ -20,7 +36,54 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await prisma.user.findUnique({ where: { username } });
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+
+      return done(null, user);
+    } catch (error) {
+      return done(error);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await prisma.user.findUnique({ where: { id } });
+
+    done(null, user);
+  } catch (error) {
+    done(error);
+  }
+});
+
+app.use("/", homeRouter);
 
 app.listen(port, (err) => {
   if (err) {
