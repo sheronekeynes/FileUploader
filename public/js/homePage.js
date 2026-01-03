4880421f18e910eb5bc29a42ccb2764c28abd318
Sherone <keynessherone@gmail.com>
2026-01-03 15:07:50 +0530

    add folder structure setup

---
+++ public/js/homePage.js
@@ -0,0 +1,6 @@
+const newFolderBtn = document.getElementById("newFolderBtn");
+const newFileBtn = document.getElementById("newFileBtn");
+
+newFolderBtn.addEventListener("click", () => {
+  console.log("clicked");
+});
