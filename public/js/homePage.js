ca27d1525c1bec32c91513b170856dd13aa2c8b6
Sherone <keynessherone@gmail.com>
2026-01-03 17:16:56 +0530

    add folder modal

--- public/js/homePage.js
+++ public/js/homePage.js
@@ -1,6 +1,20 @@
-const newFolderBtn = document.getElementById("newFolderBtn");
-const newFileBtn = document.getElementById("newFileBtn");
+document.addEventListener("DOMContentLoaded", () => {
+  const overlay = document.getElementById("folderModal");
+  const folderBtn = document.getElementById("newFolderBtn");
+  const closeBtn = document.querySelector(".closeBtn");
 
-newFolderBtn.addEventListener("click", () => {
-  console.log("clicked");
+  folderBtn.addEventListener("click", () => {
+    overlay.style.display = "flex";
+  });
+
+  overlay.addEventListener("click", (e) => {
+    if (e.target === overlay) {
+      overlay.style.display = "none";
+    }
+  });
+
+  closeBtn.addEventListener("click", (e) => {
+    e.preventDefault(); // because it's inside a form
+    overlay.style.display = "none";
+  });
 });
